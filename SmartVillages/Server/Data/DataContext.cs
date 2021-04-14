95a94e1f5ac45a3cfef3e61911dd056150a46131
Antonio <antonio.sertic@vsmti.hr>
2021-04-14 14:06:20 +0200

    Initial migration
    Dodan DbContext, User i UserType modeli te pokrenuta prva migracija.

---
+++ SmartVillages/Server/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartVillages.Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartVillages.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<User> User { get; set; }
+        public DbSet<UserType> UserType { get; set; }
+
+    }
+}
---
+++ SmartVillages/Server/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartVillages.Server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Bio { get; set; }
+        public string Sex { get; set; }
+        public string OIB { get; set; }
+        public bool EmailConfirm { get; set; } = false;
+        public bool Locked { get; set; } = true;
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Number { get; set; }
+        public string SecretCode { get; set; }
+        public DateTime BirthDate { get; set; }
+        public UserType UserType { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
