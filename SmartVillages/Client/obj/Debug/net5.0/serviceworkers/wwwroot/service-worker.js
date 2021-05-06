b65f978ba92272756230f2b2ac5f55219d150ea6
Antonio <antonio.sertic@vsmti.hr>
2021-05-06 17:50:46 +0200

    NotFound Added
    Dodan Empty layout i NotFound page

--- SmartVillages/Client/obj/Debug/net5.0/serviceworkers/wwwroot/service-worker.js
+++ SmartVillages/Client/obj/Debug/net5.0/serviceworkers/wwwroot/service-worker.js
@@ -2,4 +2,4 @@
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
 self.addEventListener('fetch', () => { });
-/* Manifest version: G6BT/2fq */
+/* Manifest version: X6gmHxM2 */
